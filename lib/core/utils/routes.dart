c99ef34df5bc7f15d9443681069638d6d818d4c9
Jonathan <jonathan.lemaine@hotmail.fr>
2022-09-03 14:06:05 +0200

    add last version without correct handle project

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -25,6 +25,7 @@ class AppRouter {
     switch (settings.name) {
       //Core
       case RoutePaths.coreSplash:
+        print('verifier sur l utilisateur a une session ouvert');
         return MaterialPageRoute(builder: (_) => const SplashScreenProfD());
       case RoutePaths.coreNoInternet:
         final args = settings.arguments as Map?;
